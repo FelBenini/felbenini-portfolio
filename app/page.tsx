5c964fe822ad55047bf6ac29c0cb9960ae926052
FelBenini <felbenini@gmail.com>
2023-08-24 12:50:36 -0300

    3dCard

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.scss'
 import HeroSection from './HomeComponents/HeroSection'
+import Card from '@/components/3dCard'
 
 export default function Home() {
   return (
     <main className={styles.Main}>
       <HeroSection />
+      <Card id='frontend-skills'>
+        <div style={{width: 300, height: 150}}>Something</div>
+      </Card>
     </main>
   )
 }
