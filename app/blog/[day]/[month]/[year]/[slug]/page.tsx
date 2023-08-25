aef4013de36a72ede97ef71deba758b963279b05
FelBenini <felbenini@gmail.com>
2023-08-25 12:13:20 -0300

    Blog page

--- app/blog/[day]/[month]/[year]/[slug]/page.tsx
+++ app/blog/[day]/[month]/[year]/[slug]/page.tsx
@@ -1,6 +1,7 @@
 import MarkdownParser from '@/components/Markdown';
 import React from 'react'
 import styles from './styles.module.scss'
+import { notFound } from 'next/navigation'
 
 export const revalidate = 10;
 
@@ -13,16 +14,20 @@ type Params = {
 
 async function fetchData(day: string, month: string, year: string, slug: string) {
     const data = await fetch(`${process.env.TORTACMS_HOST}/api/post/${day}/${month}/${year}/${slug}`);
-    console.log(`${process.env.TORTACMS_HOST}/api/post/${day}/${month}/${year}/${slug}`)
+    if (data.status !== 200) {
+      return null
+    }
     const res = await data.json();
     return res;
 }
 
 export default async function BlogPost({ params }: { params: Params }) {
     const data = await fetchData(params.day, params.month, params.year, params.slug)
+    if (!data) return notFound();
   return (
     <div className={styles.postSection}>
         <h1 className={styles.bigTitle}>{data.title}</h1>
+        {data.summary ? <h5 className={styles.summary}>{data.summary}</h5> : <></>}
         <MarkdownParser source={data.content} />
     </div>
   )
