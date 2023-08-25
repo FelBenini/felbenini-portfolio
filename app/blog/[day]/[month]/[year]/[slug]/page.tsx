3b3315559e61254342dc5b3b8a805ebb92a035cf
FelBenini <felbenini@gmail.com>
2023-08-24 21:22:56 -0300

    Basic blog stuff

---
+++ app/blog/[day]/[month]/[year]/[slug]/page.tsx
@@ -0,0 +1,28 @@
+import MarkdownParser from '@/components/Markdown';
+import React from 'react'
+
+export const revalidate = 10;
+
+type Params = {
+    day: string;
+    month: string;
+    year: string;
+    slug: string;
+}
+
+async function fetchData(day: string, month: string, year: string, slug: string) {
+    const data = await fetch(`${process.env.TORTACMS_HOST}/api/post/${day}/${month}/${year}/${slug}`);
+    console.log(`${process.env.TORTACMS_HOST}/api/post/${day}/${month}/${year}/${slug}`)
+    const res = await data.json();
+    return res;
+}
+
+export default async function BlogPost({ params }: { params: Params }) {
+    const data = await fetchData(params.day, params.month, params.year, params.slug)
+  return (
+    <div>
+        <h1>{data.title}</h1>
+        <MarkdownParser source={data.content} />
+    </div>
+  )
+}
