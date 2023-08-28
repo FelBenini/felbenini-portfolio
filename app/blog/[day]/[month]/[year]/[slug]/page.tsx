5dce495248a39dafc0d88d21ea37f054d41a7f6b
FelBenini <felbenini@gmail.com>
2023-08-28 17:28:25 -0300

    Metadata and lazy loading of big image

--- app/blog/[day]/[month]/[year]/[slug]/page.tsx
+++ app/blog/[day]/[month]/[year]/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import MarkdownParser from '@/components/Markdown';
 import React from 'react'
+import { type Metadata } from 'next';
 import styles from './styles.module.scss'
 import { notFound } from 'next/navigation'
 
@@ -21,6 +22,14 @@ async function fetchData(day: string, month: string, year: string, slug: string)
     return res;
 }
 
+export async function generateMetadata({ params }: { params: Params }): Promise<Metadata> {
+  const data = await fetchData(params.day, params.month, params.year, params.slug);
+  return {
+    title: data.title,
+    description: data.summary
+  }
+}
+
 export default async function BlogPost({ params }: { params: Params }) {
     const data = await fetchData(params.day, params.month, params.year, params.slug)
     if (!data) return notFound();
@@ -28,7 +37,7 @@ export default async function BlogPost({ params }: { params: Params }) {
     <div className={styles.postSection}>
         <h1 className={styles.bigTitle}>{data.title}</h1>
         <div className={styles.imageWrapper}>
-          <img src={data.backgroundImage} alt={data.title} />
+          <img src={data.backgroundImage} alt={data.title} loading='lazy' />
         </div>
         {data.summary ? <h5 className={styles.summary}>{data.summary}</h5> : <></>}
         <MarkdownParser source={data.content} />
