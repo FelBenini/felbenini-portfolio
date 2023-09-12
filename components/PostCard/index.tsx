2c0bc6c4481c23dc0da220cb1705eb81cb779d97
FelBenini <felbenini@gmail.com>
2023-09-12 19:10:21 -0300

    Animation on post card

--- components/PostCard/index.tsx
+++ components/PostCard/index.tsx
@@ -1,5 +1,7 @@
-import styles from './styles.module.scss'
-import Link from 'next/link';
+"use client";
+import styles from './styles.module.scss';
+import { motion } from 'framer-motion';
+import { useRouter } from 'next/navigation';
 
 export type Post = {
     title: string;
@@ -11,6 +13,7 @@ export type Post = {
 }
 
 const PostCard = ({post}: {post: Post}) => {
+    const router = useRouter();
     if (post.title.length > 52) {
         post.title = post.title.slice(0, 52) + '...'
     }
@@ -18,13 +21,21 @@ const PostCard = ({post}: {post: Post}) => {
         post.summary = post.summary.slice(0, 125) + '...';
     }
   return (
-    <Link href={`/blog/${post.postUrl}`} className={styles.card}>
+    <motion.a href={`/blog/${post.postUrl}`} className={styles.card}
+      initial={{opacity: 0, translateY: '40px'}}
+      whileInView={{opacity: 1, translateY: '0px'}}
+      transition={{delay: 0.3, ease: 'easeInOut', duration: 0.4}}
+      onClick={(e) => {
+        e.preventDefault();
+        router.push(`/blog/${post.postUrl}`)
+      }}
+    >
         <div className={styles.image}>
             <img src={post.backgroundImage} alt={post.title} loading='lazy' />
         </div>
         <h4>{post.title}</h4>
         <p>{post.summary}</p>
-    </Link>
+    </motion.a>
   )
 }
 
