7d34c3b05123b7046db2ec73873371ed465358a6
FelBenini <felbenini@gmail.com>
2023-09-13 18:45:58 -0300

    Added projects scroll component

---
+++ app/HomeComponents/ProjectsScroll/index.tsx
@@ -0,0 +1,23 @@
+import { list_of_projects } from '@/app/portfolio/projects';
+import styles from './styles.module.scss';
+import ProjectCard from '@/components/ProjectCard';
+
+const ProjectsScroll = () => {
+  return (
+    <div className={styles.projectsScroll}>
+      <div className={styles.bg}></div>
+      <div className={styles.slider}>
+        <div className={styles.content}>
+          {list_of_projects.map((project) => {
+            return <ProjectCard name={project.name} img={project.image} github={project.github} description={project.description} demo={project.website} stack={project.stack.split(', ')}/>
+          })}
+          {list_of_projects.map((project) => {
+            return <ProjectCard name={project.name} img={project.image} github={project.github} description={project.description} demo={project.website} stack={project.stack.split(', ')}/>
+          })}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProjectsScroll
