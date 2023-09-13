c2777e507704630fce9281579f06c274653123ea
FelBenini <felbenini@gmail.com>
2023-09-13 10:26:53 -0300

    Stylized portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,11 +1,13 @@
 import ProjectCard from "@/components/ProjectCard";
 import { list_of_projects } from "./projects";
+import style from './styles.module.scss';
 
 export const revalidate = 10;
 
-const PortfolioPage = () => {
+const PortfolioPage = async () => {
   return (
-    <main>
+    <main className={style.portfolioPage}>
+      <h1>Portfolio</h1>
       {list_of_projects.map((project) => {
         return <ProjectCard key={project.name} name={project.name} description={project.description} github={project.github} demo={project.website} stack={project.stack.split(', ')} img={project.image}/>
       })}
