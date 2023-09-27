785325c678e28986cce65b5991b9f754edb76eea
FelBenini <felbenini@gmail.com>
2023-09-27 20:43:10 -0300

    Sending e-mail message

--- components/ContactForm/index.tsx
+++ components/ContactForm/index.tsx
@@ -4,6 +4,7 @@ import { MdEmail } from 'react-icons/md';
 import { useState } from 'react';
 import { BiSolidPaperPlane } from 'react-icons/bi';
 import Image from 'next/image';
+import { setServers } from 'dns';
 
 const ContactForm = () => {
   const [emailInfo, setEmailInfo] = useState({
@@ -13,33 +14,34 @@ const ContactForm = () => {
     message: ""
   })
   const [loading, setLoading] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const handleChange = (value: string, element: string) => {
     setEmailInfo({...emailInfo, [element]: value});
   }
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
     if (loading) {
       return;
     }
     setLoading(true);
+    e.preventDefault();
     const body = JSON.stringify({
       "email": emailInfo.email,
       "name": emailInfo.firstName,
       "lastName": emailInfo.lastName,
       "message": emailInfo.message
-    })
+    });
     try {
-      const res = await fetch('/api/mailer', {
+      await fetch('/api/mailer', {
         method: "POST",
         body: body
-      })
-      const data = await res.json()
+      });
+      setSuccess(true);
       setLoading(false);
     } catch (error) {
-      console.log("oof");
-      setLoading(false)
+      console.log(error);
+      setLoading(false);
     }
   }
 
@@ -49,13 +51,13 @@ const ContactForm = () => {
         <h2><MdEmail /> Send an E-mail</h2>
         <p>Get in touch by sending an e-mail to felbenini@gmail.com!</p>
         <span>
-          <input type="text" placeholder='First name*' onChange={(e) => handleChange(e.target.value, "firstName") } value={emailInfo.firstName} /> 
-          <input type="text" placeholder='Last name*' onChange={(e) => handleChange(e.target.value, "lastName")} value={emailInfo.lastName}  />
+          <input type="text" placeholder='First name*' required onChange={(e) => handleChange(e.target.value, "firstName") } value={emailInfo.firstName} /> 
+          <input type="text" placeholder='Last name*' required onChange={(e) => handleChange(e.target.value, "lastName")} value={emailInfo.lastName}  />
         </span>
-          <input type="email" placeholder='Your e-mail*' onChange={(e) => handleChange(e.target.value, "email")} value={emailInfo.email} />
-          <textarea style={{width: "98%", height: '230px'}} onChange={(e) => handleChange(e.target.value, "message")} value={emailInfo.message} name="message" placeholder='Your message*' />
+          <input type="email" placeholder='Your e-mail*' required onChange={(e) => handleChange(e.target.value, "email")} value={emailInfo.email} />
+          <textarea style={{width: "98%", height: '230px'}} required onChange={(e) => handleChange(e.target.value, "message")} value={emailInfo.message} name="message" placeholder='Your message*' />
           <button type="submit">
-            <BiSolidPaperPlane /> { !loading ? 'Send' : 'Sending...' }
+            <BiSolidPaperPlane /> { !loading ? success ? 'Sent!' : 'Send' : 'Sending...' }
           </button>
       </form>
       <div className={styles.illustration} >
