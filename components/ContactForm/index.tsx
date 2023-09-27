86967a5bcaf2b15d3fa44b2229f199c3016aae76
FelBenini <felbenini@gmail.com>
2023-09-27 10:56:10 -0300

    Styling on post component and contact form

--- components/ContactForm/index.tsx
+++ components/ContactForm/index.tsx
@@ -2,6 +2,7 @@
 import styles from './styles.module.scss';
 import { MdEmail } from 'react-icons/md';
 import { useState } from 'react';
+import { BiSolidPaperPlane } from 'react-icons/bi';
 
 const ContactForm = () => {
   const [emailInfo, setEmailInfo] = useState({
@@ -31,7 +32,7 @@ const ContactForm = () => {
           <input type="email" placeholder='Your e-mail' onChange={(e) => handleChange(e.target.value, "email")} value={emailInfo.email} />
           <textarea style={{width: "98%", height: '230px'}} onChange={(e) => handleChange(e.target.value, "message")} value={emailInfo.message} name="message" placeholder='Your message' />
           <button type="submit">
-            Send
+            <BiSolidPaperPlane /> Send
           </button>
       </form>
     </section>
