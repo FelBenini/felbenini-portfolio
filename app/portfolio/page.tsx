6d0f262aa6bbec76c9fd8f7551fe4bcba7e4e650
FelBenini <felbenini@gmail.com>
2023-09-28 14:36:00 -0300

    Portfolio page changes

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,17 +1,21 @@
 import ProjectCard from "@/components/ProjectCard";
 import { list_of_projects } from "./projects";
 import style from './styles.module.scss';
+import AnimationWrapper from "@/components/AnimationWrapper";
 
 export const revalidate = 10;
 
 const PortfolioPage = async () => {
   return (
-    <main className={style.portfolioPage}>
-      <h1>Portfolio</h1>
-      {list_of_projects.map((project) => {
-        return <ProjectCard key={project.name} name={project.name} description={project.description} github={project.github} demo={project.website} stack={project.stack.split(', ')} img={project.image}/>
-      })}
-    </main>
+    <AnimationWrapper>
+      <main className={style.portfolioPage}>
+        <h1>Portfolio</h1>
+        <p>Some projects that I've built to showcase my skills and proficiency on web development.</p>
+        {list_of_projects.map((project) => {
+          return <ProjectCard key={project.name} name={project.name} description={project.description} github={project.github} demo={project.website} stack={project.stack.split(', ')} img={project.image}/>
+        })}
+      </main>
+    </AnimationWrapper>
   )
 }
 
