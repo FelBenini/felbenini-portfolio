b4d685f628609d8744bf172899b7f1a19812fb7e
FelBenini <felbenini@gmail.com>
2023-09-28 14:57:09 -0300

    Fix: Changed ' to apos;

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -8,7 +8,7 @@ const PortfolioPage = async () => {
   return (
       <main className={style.portfolioPage}>
         <h1>Portfolio</h1>
-        <p>Some projects that I've built to showcase my skills and proficiency on web development.</p>
+        <p>Some projects that I&apos;ve built to showcase my skills and proficiency on web development.</p>
         {list_of_projects.map((project) => {
           return <ProjectCard key={project.name} name={project.name} description={project.description} github={project.github} demo={project.website} stack={project.stack.split(', ')} img={project.image}/>
         })}
