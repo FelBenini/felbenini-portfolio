dbda234053aee975fec690261b52f9e1a4e4b397
FelBenini <felbenini@gmail.com>
2023-09-28 14:37:40 -0300

    Portfolio page metadata

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,13 +1,11 @@
 import ProjectCard from "@/components/ProjectCard";
 import { list_of_projects } from "./projects";
 import style from './styles.module.scss';
-import AnimationWrapper from "@/components/AnimationWrapper";
 
 export const revalidate = 10;
 
 const PortfolioPage = async () => {
   return (
-    <AnimationWrapper>
       <main className={style.portfolioPage}>
         <h1>Portfolio</h1>
         <p>Some projects that I've built to showcase my skills and proficiency on web development.</p>
@@ -15,7 +13,6 @@ const PortfolioPage = async () => {
           return <ProjectCard key={project.name} name={project.name} description={project.description} github={project.github} demo={project.website} stack={project.stack.split(', ')} img={project.image}/>
         })}
       </main>
-    </AnimationWrapper>
   )
 }
 
