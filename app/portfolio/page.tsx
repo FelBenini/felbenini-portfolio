0cc45ecca566bf8228398ad2294bdfce30ad4fba
FelBenini <felbenini@gmail.com>
2023-10-24 20:24:47 -0300

    Added animation wrapper to portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,6 +2,7 @@ import ProjectCard from "@/components/ProjectCard";
 import { list_of_projects } from "./projects";
 import style from './styles.module.scss';
 import { Metadata } from "next";
+import AnimationWrapper from "@/components/AnimationWrapper";
 
 export const metadata: Metadata = {
   title: 'Portfolio - Felipe Benini'
@@ -9,15 +10,17 @@ export const metadata: Metadata = {
 
 const PortfolioPage = async () => {
   return (
-    <div className={style.bigBg}>
-      <main className={style.portfolioPage}>
-        <h1>Portfolio</h1>
-        <p>Some projects that I&apos;ve built to showcase my skills and proficiency on web development.</p>
-        {list_of_projects.map((project) => {
-          return <ProjectCard key={project.name} name={project.name} description={project.description} github={project.github} demo={project.website} stack={project.stack.split(', ')} img={project.image}/>
-        })}
-      </main>
-    </div>
+    <AnimationWrapper>
+      <div className={style.bigBg}>
+        <main className={style.portfolioPage}>
+          <h1>Portfolio</h1>
+          <p>Some projects that I&apos;ve built to showcase my skills and proficiency on web development.</p>
+          {list_of_projects.map((project) => {
+            return <ProjectCard key={project.name} name={project.name} description={project.description} github={project.github} demo={project.website} stack={project.stack.split(', ')} img={project.image}/>
+          })}
+        </main>
+      </div>
+    </AnimationWrapper>
   )
 }
 
